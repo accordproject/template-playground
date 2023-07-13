bb9babf55a9c20f5bc9079d5ce1f4e7ee18c7a13
Dan Selman <danscode@selman.org>
2023-07-13 15:31:48 +0100

    fix(vite) - production bundle works
    Signed-off-by: Dan Selman <danscode@selman.org>

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'vite-plugin-node-stdlib-browser'
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
-    react(),
-    nodeResolve({ preferBuiltins: false }),
-    commonjs(),
+    react()
   ]
 })
