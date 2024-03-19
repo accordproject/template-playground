3d24d2d7e32ccec3597d14d93ccdb9e265edaba5
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2024-03-19 22:22:00 +0100

    feat: added new load samples
    Signed-off-by: Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,30 +1,32 @@
-import * as playground from './playground';
-import * as helloworld from './helloworld';
-import * as formulanow from './formulanow';
-import * as join from './join';
-import * as clause from './clause';
-import * as list from './list';
-import * as optional from './optional';
-import * as markdown from './markdown';
-import * as formula from './formula';
-import * as clausecondition from './clausecondition';
+import * as playground from "./playground";
+import * as helloworld from "./helloworld";
+import * as formulanow from "./formulanow";
+import * as join from "./join";
+import * as clause from "./clause";
+import * as list from "./list";
+import * as optional from "./optional";
+import * as markdown from "./markdown";
+import * as formula from "./formula";
+import * as clausecondition from "./clausecondition";
+import * as invitation from "./invitation";
 
 export type Sample = {
-    NAME: string,
-    MODEL: string,
-    TEMPLATE: string,
-    DATA: object
-}
+  NAME: string;
+  MODEL: string;
+  TEMPLATE: string;
+  DATA: object;
+};
 
-export const SAMPLES:Array<Sample> = [
-    playground,
-    helloworld,
-    formula,
-    formulanow,
-    join,
-    clause,
-    clausecondition,
-    list,
-    optional,
-    markdown
+export const SAMPLES: Array<Sample> = [
+  playground,
+  helloworld,
+  formula,
+  formulanow,
+  join,
+  clause,
+  clausecondition,
+  invitation,
+  list,
+  optional,
+  markdown,
 ];
