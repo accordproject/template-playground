d94f5cf7f9b1128087cab6c9a532f72d518b3aea
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-01 08:36:48 +0100

    Revert

--- src/JSONEditor.tsx
+++ src/JSONEditor.tsx
@@ -1,18 +1,1 @@
-import * as monaco from '@monaco-editor/react';
-import { editor } from 'monaco-editor';
 
-const options:editor.IStandaloneEditorConstructionOptions = {
-  minimap: { enabled: false },
-  wordWrap: "on",
-  automaticLayout:true
-}
-
-export default function JSONEditor( {value, onChange} : {value: string, onChange?: (value:string|undefined) => void} ) {
-  
-    return (
-    <div className="editorwrapper">
-        <monaco.Editor options={ options }
-        language='json' height="60vh" value={value} onChange={onChange} />
-    </div>
-  );
-}
