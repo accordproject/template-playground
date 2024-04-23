c7e692d4761378eef94f2f9b741d4e876d134a35
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-23 11:07:28 +0100

    update: ui development kickoff
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react";
 import { App as AntdApp, Typography, Col, Collapse, Row } from "antd";
 import { Layout, theme } from "antd";
 const { Header, Content } = Layout;
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from "semantic-ui-react";
 
 import AgreementData from "./AgreementData";
 import AgreementHtml from "./AgreementHtml";
@@ -51,7 +61,7 @@ function App() {
     <AntdApp>
       <Layout>
         <Layout>
-          <Header
+          {/* <Header
             style={{
               textAlign: "center",
               padding: 0,
@@ -62,7 +72,44 @@ function App() {
               Template Playground{" "}
               <span style={{ fontSize: "80%", color: "#87CEEB" }}>(BETA)</span>
             </Typography.Title>
-          </Header>
+          </Header> */}
+          <Menu
+            fixed="top"
+            inverted
+            style={{ background: "#1b2540", height: "65px" }}
+          >
+            <Container>
+              <Menu.Item as="a" header>
+                <Image
+                  size="small"
+                  href="https://www.accordproject.org"
+                  src="/logo.png"
+                  style={{ marginRight: "1.5em" }}
+                  target="_blank"
+                />
+                Project Name
+              </Menu.Item>
+              <Menu.Item as="a">Home</Menu.Item>
+
+              <Dropdown item simple text="Dropdown">
+                <Dropdown.Menu>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Header Item</Dropdown.Header>
+                  <Dropdown.Item>
+                    <i className="dropdown icon" />
+                    <span className="text">Submenu</span>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>List Item</Dropdown.Item>
+                      <Dropdown.Item>List Item</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Container>
+          </Menu>
           <Content>
             <div
               style={{
