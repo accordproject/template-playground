0ea1b359bafb9020f1a35e92a9c481e90794fca1
sanketshevkar <shevkar.sanket@gmail.com>
2024-06-02 19:51:17 +0530

    chore(test): added vitest with RTL
    Signed-off-by: sanketshevkar <shevkar.sanket@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [
     nodePolyfills(),
-    react()
-  ]
-})
+    react()],
+});
+
+// https://vitest.dev/config/
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './utils/testing/setup.ts',
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
