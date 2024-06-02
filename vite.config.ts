1d8e979371f17eb0ce80fbe785118b9484f4f71b
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-02 13:05:29 +0100

    chore: updated vite config
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,4 @@ import nodePolyfills from "vite-plugin-node-stdlib-browser";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
-  build: {
-    chunkSizeWarningLimit: 100,
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
-      },
-    },
-  },
 });
