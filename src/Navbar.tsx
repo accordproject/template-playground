b6ed3f8fcf303719de3995c80e3e39c190bf270d
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-05 14:39:31 +0100

    dev: semanticui >> antd
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,73 +1,149 @@
+import { Menu, Dropdown, Button, Image } from "antd";
 import {
-  Container,
-  Dropdown,
-  Header,
-  Icon,
-  Image,
-  Menu,
-} from "semantic-ui-react";
+  GithubOutlined,
+  QuestionOutlined,
+  UserOutlined,
+  InfoOutlined,
+  BookOutlined,
+  CaretDownFilled,
+} from "@ant-design/icons";
 
 function Navbar({ scrollToExplore }: { scrollToExplore: any }) {
+  const menu = (
+    <Menu>
+      <Menu.ItemGroup title="Info">
+        <Menu.Item key="about">
+          <a
+            href="https://github.com/accordproject/template-playground/blob/main/README.md"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <QuestionOutlined /> About
+          </a>
+        </Menu.Item>
+        <Menu.Item key="community">
+          <a
+            href="https://discord.com/invite/Zm99SKhhtA"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <UserOutlined /> Community
+          </a>
+        </Menu.Item>
+        <Menu.Item key="issues">
+          <a
+            href="https://github.com/accordproject/template-playground/issues"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InfoOutlined /> Issues
+          </a>
+        </Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.ItemGroup title="Documentation">
+        <Menu.Item key="documentation">
+          <a
+            href="https://github.com/accordproject/template-engine/blob/main/README.md"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BookOutlined /> Documentation
+          </a>
+        </Menu.Item>
+      </Menu.ItemGroup>
+    </Menu>
+  );
+
   return (
     <Menu
-      fixed="top"
-      inverted
-      style={{ background: "#1b2540", height: "65px" }}
+      mode="horizontal"
+      theme="dark"
+      style={{
+        background: "#1b2540",
+        height: "65px",
+        lineHeight: "65px",
+        paddingLeft: 40,
+        paddingRight: 40,
+        display: "flex",
+        alignItems: "center",
+      }}
     >
-      <Container>
-        <Menu.Item as="a" header>
+      <Menu.Item
+        key="home"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          borderRight: "1.5px solid rgba(255, 255, 255, 0.1)",
+        }}
+      >
+        <a
+          href="https://www.accordproject.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Image
-            size="small"
-            href="https://www.accordproject.org"
             src="/logo.png"
-            style={{ marginRight: "1.5em" }}
-            target="_blank"
+            preview={false}
+            style={{ paddingRight: "1.5em", height: "26px" }}
           />
-          Template Playground{" "}
-        </Menu.Item>
-        <Menu.Item as="a" onClick={scrollToExplore}>
-          Explore
-        </Menu.Item>
-
-        <Dropdown item simple text="Help">
-          <Dropdown.Menu>
-            <Header as="h4">Info</Header>
-            <Menu.Item
-              href="https://github.com/accordproject/template-playground/blob/main/README.md"
-              target="_blank"
-            >
-              <Icon name="question" /> About
-            </Menu.Item>
-            <Menu.Item
-              href="https://discord.com/invite/Zm99SKhhtA"
-              target="_blank"
-            >
-              <Icon name="user" /> Community
-            </Menu.Item>
-            <Menu.Item
-              href="https://github.com/accordproject/template-playground/issues"
-              target="_blank"
-            >
-              <Icon name="info" /> Issues
-            </Menu.Item>
-            <Header as="h4">Documentation</Header>
-            <Menu.Item
-              href="https://github.com/accordproject/template-engine/blob/main/README.md"
-              target="_blank"
-            >
-              <Icon name="book" /> Documentaion
-            </Menu.Item>
-          </Dropdown.Menu>
+        </a>
+        <span style={{ color: "white" }}>Template Playground</span>
+      </Menu.Item>
+      <Menu.Item
+        key="explore"
+        onClick={scrollToExplore}
+        style={{
+          height: "65px",
+          display: "flex",
+          alignItems: "center",
+          borderRight: "1.5px solid rgba(255, 255, 255, 0.1)",
+        }}
+      >
+        Explore
+      </Menu.Item>
+      <Menu.Item
+        style={{
+          height: "65px",
+          display: "flex",
+          alignItems: "center",
+          borderRight: "1.5px solid rgba(255, 255, 255, 0.1)",
+        }}
+      >
+        <Dropdown overlay={menu}>
+          <Button
+            style={{
+              background: "transparent",
+              border: "none",
+              color: "white",
+              height: "65px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            Help
+            <CaretDownFilled style={{ fontSize: "10px" }} />
+          </Button>
         </Dropdown>
-        <Menu.Item
-          as="a"
-          position="right"
+      </Menu.Item>
+      <Menu.Item
+        key="github"
+        style={{
+          marginLeft: "auto",
+          height: "65px",
+          display: "flex",
+          alignItems: "center",
+          borderLeft: "1.5px solid rgba(255, 255, 255, 0.1)",
+        }}
+      >
+        <a
           href="https://github.com/accordproject/template-playground"
           target="_blank"
+          rel="noopener noreferrer"
         >
-          <Icon name="github" size="large" style={{ color: "white" }} /> Github
-        </Menu.Item>
-      </Container>
+          <GithubOutlined style={{ fontSize: "24px", color: "white" }} />
+        </a>
+        <span style={{ color: "white" }}>Github</span>
+      </Menu.Item>
     </Menu>
   );
 }
