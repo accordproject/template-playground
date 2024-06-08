6a681e3ddb40519dfce12953de9b57d5f60d790a
Sanket Shevkar <43399522+sanketshevkar@users.noreply.github.com>
2024-06-08 15:25:57 +0530

    Revert "chore(test): added vitest with RTL (#64)"
    This reverts commit 7e5cbb0e6e492cd246a98bfaa5e4cc4a86bf0881.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
-import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
-import { defineConfig as defineVitestConfig } from 'vitest/config';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
-const viteConfig = defineViteConfig({
+export default defineConfig({
   plugins: [
     nodePolyfills(),
-    react()],
-});
-
-// https://vitest.dev/config/
-const vitestConfig = defineVitestConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './utils/testing/setup.ts',
-  },
-});
-
-export default mergeConfig(viteConfig, vitestConfig);
+    react()
+  ]
+})
