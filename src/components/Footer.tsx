fdda60eca2a2ab25fcc09bc0c42ac33311ebdd98
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-12 03:58:50 +0100

    update: development
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,15 +14,21 @@ const footerSections = [
   {
     title: "ABOUT",
     links: [
-      { title: "About the AP", href: "/about" },
-      { title: "FAQ", href: "/faq" },
+      { title: "About the AP", href: "https://accordproject.org/about/" },
+      {
+        title: "FAQ",
+        href: "https://accordproject.org/frequently-asked-questions/",
+      },
     ],
   },
   {
     title: "COMMUNITY",
     links: [
-      { title: "Contribute", href: "/contribute" },
-      { title: "Tech WG", href: "/tech-wg" },
+      { title: "Contribute", href: "https://accordproject.org/contribute/" },
+      {
+        title: "Tech WG",
+        href: "https://accordproject.org/working-groups/technology/",
+      },
       {
         title: "Join Discord",
         href: "https://discord.com/invite/Zm99SKhhtA",
@@ -33,26 +39,32 @@ const footerSections = [
   {
     title: "PROJECTS",
     links: [
-      { title: "Cicero", href: "/cicero" },
-      { title: "Ergo", href: "/ergo" },
-      { title: "Concerto", href: "/concerto" },
-      { title: "Template Studio", href: "/template-studio" },
+      {
+        title: "Concerto",
+        href: "https://accordproject.org/projects/concerto/",
+      },
+      { title: "Cicero", href: "https://accordproject.org/projects/cicero/" },
+      {
+        title: "VS Code Extension",
+        href: "https://marketplace.visualstudio.com/items?itemName=accordproject.cicero-vscode-extension",
+      },
     ],
   },
   {
     title: "RESOURCES",
     links: [
-      { title: "Template Studio", href: "/template-studio" },
-      { title: "Template Library", href: "/template-library" },
-      { title: "Model Repository", href: "/model-repository" },
-      { title: "VS Code Extension", href: "/vscode-extension" },
-      { title: "Videos", href: "/videos" },
+      {
+        title: "Template Library",
+        href: "https://templates.accordproject.org/",
+      },
+      { title: "Model Repository", href: "https://models.accordproject.org/" },
+      { title: "Videos", href: "https://vimeo.com/accordproject" },
       {
         title: "GitHub",
         href: "https://github.com/accordproject",
         external: true,
       },
-      { title: "Documentation", href: "/documentation" },
+      { title: "Documentation", href: "https://docs.accordproject.org/" },
     ],
   },
 ];
@@ -86,17 +98,21 @@ const CustomFooter: React.FC = () => {
             <Text style={{ color: "rgba(255, 255, 255, 0.65)" }}>
               The open source smart legal contract stack
             </Text>
-            <Text style={{ color: "rgba(255, 255, 255, 0.65)" }}>
-              admin@accordproject.org
-            </Text>
-            <Button
-              type="primary"
-              shape="round"
-              size="large"
-              style={{ padding: "2px 24px" }}
-            >
-              Join
-            </Button>
+            <Link href="mailto:admin@accordproject.org">
+              <Text strong style={{ color: "rgba(255, 255, 255, 0.65)" }}>
+                admin@accordproject.org
+              </Text>
+            </Link>
+            <Link href="https://discord.com/invite/Zm99SKhhtA" target="_blank">
+              <Button
+                type="primary"
+                shape="round"
+                size="large"
+                style={{ padding: "2px 24px" }}
+              >
+                Join
+              </Button>
+            </Link>
           </Space>
         </Col>
         {footerSections.map((section) => (
