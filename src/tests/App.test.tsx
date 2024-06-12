43a3d220a170bbcf610c3daeceb97bb81ab261ea
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-12 04:32:05 +0100

    update: development
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,7 @@
 import { render } from "@testing-library/react";
 
-import Footer from "../components/Footer";
-
-describe("Footer", () => {
-  it("renders Footer", () => {
-    render(<Footer />);
+describe("App", () => {
+  it("renders div", () => {
+    render(<div>App</div>);
   });
 });
