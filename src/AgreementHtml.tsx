f5f0a5b914bc60c1da7aec734053f1ffb335e463
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-20 16:19:07 +0100

    update: development
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/AgreementHtml.tsx
+++ src/AgreementHtml.tsx
@@ -1,12 +1,30 @@
-import useAppStore from './store';
+import useAppStore from "./store";
 
 function AgreementHtml() {
-  const agreementHtml = useAppStore((state) => state.agreementHtml)
-  return <div className="column">
-    <h2>Preview Ouput</h2>
-    <p>The result of merging the JSON data with the template. This is AgreementMark converted to HTML.</p>
-    <div className="agreement" dangerouslySetInnerHTML={{ __html: agreementHtml }}/>
-  </div>;
+  const agreementHtml = useAppStore((state) => state.agreementHtml);
+
+  return (
+    <div
+      className="column"
+      style={{
+        border: "1px solid #d9d9d9",
+        borderRadius: "8px",
+        padding: "16px 16px 100px 16px",
+        height: "calc(100vh - 64px)",
+        overflowY: "auto",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}>Preview Output</h2>
+      <p>
+        The result of merging the JSON data with the template. This is
+        AgreementMark converted to HTML.
+      </p>
+      <div
+        className="agreement"
+        dangerouslySetInnerHTML={{ __html: agreementHtml }}
+      />
+    </div>
+  );
 }
 
 export default AgreementHtml;
