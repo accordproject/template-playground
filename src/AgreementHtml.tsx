1e7b038370b6b238653c9e91a6784f09ddbbc0d7
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-20 16:36:29 +0100

    dev: load state
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/AgreementHtml.tsx
+++ src/AgreementHtml.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+import { Spin } from "antd";
 import useAppStore from "./store";
 
 function AgreementHtml() {
   const agreementHtml = useAppStore((state) => state.agreementHtml);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (agreementHtml) {
+      setLoading(false);
+    }
+  }, [agreementHtml]);
 
   return (
     <div
@@ -9,20 +18,38 @@ function AgreementHtml() {
       style={{
         border: "1px solid #d9d9d9",
         borderRadius: "8px",
-        padding: "16px 16px 100px 16px",
+        padding: "16px",
         height: "calc(100vh - 64px)",
         overflowY: "auto",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <h2 style={{ textAlign: "center" }}>Preview Output</h2>
-      <p>
-        The result of merging the JSON data with the template. This is
-        AgreementMark converted to HTML.
-      </p>
-      <div
-        className="agreement"
-        dangerouslySetInnerHTML={{ __html: agreementHtml }}
-      />
+      <div style={{ textAlign: "center" }}>
+        <h2>Preview Output</h2>
+        <p>
+          The result of merging the JSON data with the template. This is
+          AgreementMark converted to HTML.
+        </p>
+      </div>
+      {loading ? (
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Spin tip="Loading..." size="large" />
+        </div>
+      ) : (
+        <div
+          className="agreement"
+          dangerouslySetInnerHTML={{ __html: agreementHtml }}
+          style={{ flex: 1 }}
+        />
+      )}
     </div>
   );
 }
