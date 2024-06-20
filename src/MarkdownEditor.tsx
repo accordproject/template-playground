93e845cc4198007128886c64d53f74d91bc7ef4f
Mahesh Patil <nitro56565@gmail.com>
2024-06-20 17:24:11 +0530

    Fixed whitspace and formatting
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>

--- src/MarkdownEditor.tsx
+++ src/MarkdownEditor.tsx
@@ -1,21 +1,18 @@
 import * as monaco from '@monaco-editor/react';
 import { editor } from 'monaco-editor';
 
-const options: editor.IStandaloneEditorConstructionOptions = {
+const options:editor.IStandaloneEditorConstructionOptions = {
   minimap: { enabled: false },
-  wordWrap: 'on',
+  wordWrap: "on",
+  automaticLayout:true,
   scrollBeyondLastLine: false,
-};
+}
 
-export default function MarkdownEditor({ value, onChange }: { value: string, onChange?: (value: string | undefined) => void }) {
+export default function MarkdownEditor( {value, onChange} : {value: string, onChange?: (value:string|undefined) => void} ) {
   return (
     <div className="editorwrapper">
-      <monaco.Editor
-        options={options}
-        height="60vh"
-        value={value}
-        onChange={onChange}
-      />
+        <monaco.Editor options={ options } 
+        language='markdown' height="60vh" value={value} onChange={onChange}/>
     </div>
   );
 }
