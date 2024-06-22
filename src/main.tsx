f0616b0e08c5672d38e0d28473d7f630ee0298f7
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-22 08:44:53 +0100

    feat: cross-browser compatibility  (#77)
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    ---------
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
-import 'regenerator-runtime'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
