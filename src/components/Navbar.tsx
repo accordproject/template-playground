83069078259efc3818ccc7b7a7ddb924393a7d77
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-25 12:23:11 +0100

    update: development
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Menu, Dropdown, Button, Image } from "antd";
+import { Menu, Dropdown, Button, Image, Grid } from "antd";
 import {
   GithubOutlined,
   QuestionOutlined,
@@ -9,10 +9,13 @@ import {
   CaretDownFilled,
 } from "@ant-design/icons";
 
+const { useBreakpoint } = Grid;
+
 function Navbar({ scrollToExplore }: { scrollToExplore: any }) {
   const [hovered, setHovered] = useState<
     null | "home" | "explore" | "help" | "github"
   >(null);
+  const screens = useBreakpoint();
 
   const menu = (
     <Menu>
@@ -59,32 +62,34 @@ function Navbar({ scrollToExplore }: { scrollToExplore: any }) {
     </Menu>
   );
 
-  const menuItemStyle = (key: string) => ({
+  const menuItemStyle = (key: string, isLast: boolean) => ({
     display: "flex",
     alignItems: "center",
-    borderRight: "1.5px solid rgba(255, 255, 255, 0.1)",
+    padding: screens.md ? "0 20px" : "0",
     backgroundColor:
       hovered === key ? "rgba(255, 255, 255, 0.1)" : "transparent",
     height: "65px",
+    borderRight:
+      screens.md && !isLast ? "1.5px solid rgba(255, 255, 255, 0.1)" : "none",
   });
 
   return (
-    <Menu
-      mode="horizontal"
-      theme="dark"
+    <div
       style={{
         background: "#1b2540",
         height: "65px",
         lineHeight: "65px",
         display: "flex",
         alignItems: "center",
-        paddingLeft: 40,
-        paddingRight: 40,
+        paddingLeft: screens.md ? 40 : 10,
+        paddingRight: screens.md ? 40 : 10,
       }}
     >
-      <Menu.Item
-        key="home"
-        style={menuItemStyle("home")}
+      <div
+        style={{
+          cursor: "pointer",
+          ...menuItemStyle("home", false),
+        }}
         onMouseEnter={() => setHovered("home")}
         onMouseLeave={() => setHovered(null)}
       >
@@ -92,57 +97,77 @@ function Navbar({ scrollToExplore }: { scrollToExplore: any }) {
           href="https://www.accordproject.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ display: "flex", alignItems: "center" }}
         >
           <Image
-            src="/logo.png"
+            src={screens.md ? "/logo.png" : "/accord_logo.png"}
             alt="Template Playground"
             preview={false}
-            style={{ paddingRight: "1.5em", height: "26px" }}
-          />
-        </a>
-        <span style={{ color: "white" }}>Template Playground</span>
-      </Menu.Item>
-      <Menu.Item
-        key="explore"
-        onClick={scrollToExplore}
-        style={menuItemStyle("explore")}
-        onMouseEnter={() => setHovered("explore")}
-        onMouseLeave={() => setHovered(null)}
-      >
-        Explore
-      </Menu.Item>
-      <Menu.Item
-        key="help"
-        style={menuItemStyle("help")}
-        onMouseEnter={() => setHovered("help")}
-        onMouseLeave={() => setHovered(null)}
-      >
-        <Dropdown overlay={menu} trigger={["click"]}>
-          <Button
             style={{
-              background: "transparent",
-              border: "none",
-              color: "white",
-              height: "65px",
-              display: "flex",
-              alignItems: "center",
+              paddingRight: screens.md ? "1.5em" : "10px",
+              height: "26px",
             }}
+          />
+          <span style={{ color: "white" }}>Template Playground</span>
+        </a>
+      </div>
+      {screens.md && (
+        <>
+          <div
+            style={{
+              ...menuItemStyle("explore", false),
+              cursor: "pointer",
+            }}
+            onClick={scrollToExplore}
+            onMouseEnter={() => setHovered("explore")}
+            onMouseLeave={() => setHovered(null)}
           >
-            Help
-            <CaretDownFilled style={{ fontSize: "10px", marginLeft: "5px" }} />
-          </Button>
-        </Dropdown>
-      </Menu.Item>
-      <Menu.Item
-        key="github"
+            <span style={{ color: "white" }}>Explore</span>
+          </div>
+          <div
+            style={{
+              ...menuItemStyle("help", false),
+              cursor: "pointer",
+            }}
+            onMouseEnter={() => setHovered("help")}
+            onMouseLeave={() => setHovered(null)}
+          >
+            <Dropdown overlay={menu} trigger={["click"]}>
+              <Button
+                style={{
+                  background: "transparent",
+                  border: "none",
+                  color: "white",
+                  height: "65px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                Help
+                <CaretDownFilled
+                  style={{ fontSize: "10px", marginLeft: "5px" }}
+                />
+              </Button>
+            </Dropdown>
+          </div>
+        </>
+      )}
+      <div
         style={{
-          marginLeft: "auto",
+          marginLeft: screens.md ? "auto" : "unset",
           height: "65px",
           display: "flex",
           alignItems: "center",
-          borderLeft: "1.5px solid rgba(255, 255, 255, 0.1)",
+          borderLeft: screens.md
+            ? "1.5px solid rgba(255, 255, 255, 0.1)"
+            : "none",
+          paddingLeft: screens.md ? "20px" : "0",
           backgroundColor:
             hovered === "github" ? "rgba(255, 255, 255, 0.1)" : "transparent",
+          cursor: "pointer",
+          position: screens.md ? "relative" : "absolute",
+          right: screens.md ? "auto" : "10px",
+          paddingRight: screens.md ? 0 : "10px",
         }}
         onMouseEnter={() => setHovered("github")}
         onMouseLeave={() => setHovered(null)}
@@ -154,12 +179,16 @@ function Navbar({ scrollToExplore }: { scrollToExplore: any }) {
           style={{ display: "flex", alignItems: "center", color: "white" }}
         >
           <GithubOutlined
-            style={{ fontSize: "20px", color: "white", marginRight: "5px" }}
+            style={{
+              fontSize: "20px",
+              color: "white",
+              marginRight: screens.md ? "5px" : "0",
+            }}
           />
-          <span>Github</span>
+          {screens.md && <span>Github</span>}
         </a>
-      </Menu.Item>
-    </Menu>
+      </div>
+    </div>
   );
 }
 
