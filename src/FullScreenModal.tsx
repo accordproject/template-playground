900b44f1d6d6141d122fc40e0d505449e7c9741c
Mahesh Patil <104414141+nitro56565@users.noreply.github.com>
2024-07-10 18:03:13 +0530

    full screen modal implemented (#83)
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>

---
+++ src/FullScreenModal.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import AgreementHtml from './AgreementHtml';
+import { FullscreenOutlined } from '@ant-design/icons';
+
+const FullScreenModal: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div style={{ textAlign: 'right' }}>
+      <FullscreenOutlined 
+        style={{ fontSize: '24px', cursor: 'pointer' , marginRight: '10px'}} 
+        onClick={() => setOpen(true)} 
+      />
+      <Modal
+        title="Output"
+        centered
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={1000}
+      >
+        <AgreementHtml loading={false} />
+      </Modal>
+    </div>
+  );
+};
+
+export default FullScreenModal;
