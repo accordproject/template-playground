74a360f21ce19138e29de2a21201af7564d51f38
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-07-26 10:31:34 +0100

    test: compression tests
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

---
+++ src/tests/utils/compression/Compression.test.tsx
@@ -0,0 +1,26 @@
+// src/utils/compression/compression.test.ts
+import {
+  compress,
+  decompress,
+} from "../../../../utils/compression/compression";
+
+describe("Compression Utilities", () => {
+  it("should correctly compress and decompress data", () => {
+    const data = {
+      templateMarkdown: "Sample Template",
+      modelCto: "Sample Model",
+      data: '{"sample": "data"}',
+    };
+
+    const compressed = compress(data);
+    const decompressed = decompress(compressed);
+
+    expect(decompressed).toEqual(data);
+  });
+
+  it("should throw an error if decompression fails", () => {
+    expect(() => {
+      decompress("invalid data");
+    }).toThrow("Failed to decompress data");
+  });
+});
