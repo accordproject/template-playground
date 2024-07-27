101fabdcd1706f593e109ed0091f600108e04851
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-07-27 19:47:24 +0100

    chore(refactor): enhanced general project structure (#88)
    * update: store into stores folder
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update(editors): development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * update: development
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * chore: utils >>> src
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * chore: dev fixes
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    * fix: reslove issues
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>
    
    ---------
    
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

--- src/editors/editorsContainer/AgreementData.tsx
+++ src/editors/editorsContainer/AgreementData.tsx
@@ -1,5 +1,5 @@
-import JSONEditor from "./JSONEditor";
-import useAppStore from "./store";
+import JSONEditor from "../JSONEditor";
+import useAppStore from "../../store/store";
 
 function AgreementData() {
   const agreementData = useAppStore((state) => state.data);
