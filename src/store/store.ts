6e81fd0a2bba893679d465cc33bc65de116e31ce
Mahesh Patil <104414141+nitro56565@users.noreply.github.com>
2024-08-03 00:11:59 +0530

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,3 @@
-// Update the store with editorModelCto and setEditorModelCto
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
