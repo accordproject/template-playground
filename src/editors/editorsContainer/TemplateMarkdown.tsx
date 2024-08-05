b74b5facaba7d5d61c8aefad886dd63dd280ca96
Mahesh Patil <104414141+nitro56565@users.noreply.github.com>
2024-08-05 20:32:52 +0530

    cursor inconsistency in editor fixed (#95)
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>
    Signed-off-by: Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>

--- src/editors/editorsContainer/TemplateMarkdown.tsx
+++ src/editors/editorsContainer/TemplateMarkdown.tsx
@@ -1,24 +1,35 @@
 import MarkdownEditor from "../MarkdownEditor";
 import useAppStore from "../../store/store";
+import { useCallback } from 'react';
+import { debounce } from "ts-debounce";
 
 function TemplateMarkdown() {
-  const templateMarkdown = useAppStore((state) => state.templateMarkdown);
+  const editorValue = useAppStore((state) => state.editorValue);
+  const setEditorValue = useAppStore((state) => state.setEditorValue);
   const setTemplateMarkdown = useAppStore((state) => state.setTemplateMarkdown);
 
-  function onChange(value: string | undefined) {
-    if (value) {
+  const debouncedSetTemplateMarkdown = useCallback(
+    debounce((value: string) => {
       void setTemplateMarkdown(value);
+    }, 500),
+    []
+  );
+
+  const handleChange = (value: string | undefined) => {
+    if (value !== undefined) {
+      setEditorValue(value);
+      debouncedSetTemplateMarkdown(value);
     }
-  }
+  };
 
   return (
     <div className="column">
       <h2>TemplateMark</h2>
       <p>
         A natural language template with embedded variables, conditional
-        sections and TypeScript code.
+        sections, and TypeScript code.
       </p>
-      <MarkdownEditor value={templateMarkdown} onChange={onChange} />
+      <MarkdownEditor value={editorValue} onChange={handleChange} />
     </div>
   );
 }
