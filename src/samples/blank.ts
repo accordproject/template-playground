5ee7ef56fed6011b0c83888216e19ffb206bcfd7
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-09-30 00:07:42 -0700

    update: added blank(new) template sample (#110)
    Signed-off-by: Vinyl-Davyl <okononfuadavid@gmail.com>

---
+++ src/samples/blank.ts
@@ -0,0 +1,21 @@
+const MODEL = `namespace hello@1.0.0
+
+@template
+concept NewTemplate {
+    o String exampleField
+}`;
+
+const TEMPLATE = `> This is a blank template for users to create their own content.
+
+### Welcome to your new template {{exampleField}}!
+
+You can add your content here.
+`;
+
+const DATA = {
+    "$class": "hello@1.0.0.NewTemplate",
+    "exampleField": "Your value here"
+};
+
+const NAME = 'New (Blank)';
+export { NAME, MODEL, DATA, TEMPLATE };
