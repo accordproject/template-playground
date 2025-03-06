9c61e672d6d244f0541b418747d52924c7023d82
surajgupt01 <surajgupt880@gmail.com>
2025-03-07 00:28:32 +0530

    Responsiveness of navbar at learn route fixed for small devices
    Signed-off-by: surajgupt01 <surajgupt880@gmail.com>

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,14 +12,41 @@ import {
   DividerLine,
 } from "../styles/components/Sidebar";
 import { BulbOutlined } from "@ant-design/icons";
+import { useState , useEffect } from "react";
+import { Collapse } from "antd";
+
+
+
+
 
 const Sidebar: React.FC<{ steps: { title: string; link: string }[] }> = ({
   steps,
 }) => {
+
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <SidebarContainer>
-      <SidebarTitle>Learning Pathway</SidebarTitle>
+
+
+    <>
+    {isMobile ? (    <Collapse>
+     <Collapse.Panel key={"1"} header={""}>
+
+    <SidebarContainer >
+    
+  
       <SidebarList>
+      <SidebarTitle>Learning Pathway</SidebarTitle>
         {steps.map((step, index) => (
           <SidebarListItem key={index}>
             <SidebarLink
@@ -31,6 +58,8 @@ const Sidebar: React.FC<{ steps: { title: string; link: string }[] }> = ({
           </SidebarListItem>
         ))}
       </SidebarList>
+  
+
       <DividerLine />
       <HelperBox>
         <HelperIcon>
@@ -45,8 +74,56 @@ const Sidebar: React.FC<{ steps: { title: string; link: string }[] }> = ({
           in another tab to experiment as you learn.
         </HelperText>
       </HelperBox>
+
     </SidebarContainer>
+
+    </Collapse.Panel>
+    </Collapse>) : (    
+
+    <SidebarContainer >
+    
+  
+      <SidebarList>
+      <SidebarTitle>Learning Pathway</SidebarTitle>
+        {steps.map((step, index) => (
+          <SidebarListItem key={index}>
+            <SidebarLink
+              to={step.link}
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+            >
+              {step.title}
+            </SidebarLink>
+          </SidebarListItem>
+        ))}
+      </SidebarList>
+  
+
+      <DividerLine />
+      <HelperBox>
+        <HelperIcon>
+          <BulbOutlined />
+        </HelperIcon>
+        <HelperText>
+          Welcome to the Learning Pathway! Use the sidebar to follow the guide.
+          Open the{" "}
+          <Link to="/" target="_blank" rel="noopener noreferrer">
+            Template Playground
+          </Link>{" "}
+          in another tab to experiment as you learn.
+        </HelperText>
+      </HelperBox>
+
+    </SidebarContainer>
+
+)}
+    </>
+
+
+
+  
   );
 };
 
 export default Sidebar;
+
+
