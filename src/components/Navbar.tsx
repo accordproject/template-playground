b9d20f44a053d641a38f3c93052f03feefbdaf01
dipishBisht <dipish1718@gmail.com>
2025-03-07 18:44:07 +0530

    removed deprecated package
    Signed-off-by: dipishBisht <dipish1718@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Menu, Dropdown, Button, Image, Grid } from "antd";
+import { Dropdown, Button, Image, Grid } from "antd";
 import { useSpring, animated } from "react-spring";
 import { useLocation, Link } from "react-router-dom";
 import {
@@ -22,59 +22,74 @@ function Navbar({ scrollToFooter }: { scrollToFooter: any }) {
   const location = useLocation();
 
   const props = useSpring({
-    loop: true,
+    to: async (next) => {
+      while (true) {
+        await next({
+          opacity: 1,
+          boxShadow: "0px 0px 5px rgba(255, 255, 255, 1)",
+        });
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await next({
+          opacity: 0.9,
+          boxShadow: "0px 0px 0px rgba(255, 255, 255, 0)",
+        });
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+      }
+    },
     from: { opacity: 0.5, boxShadow: "0px 0px 0px rgba(255, 255, 255, 0)" },
-    to: [
-      { opacity: 1, boxShadow: "0px 0px 5px rgba(255, 255, 255, 1)" },
-      { opacity: 0.9, boxShadow: "0px 0px 0px rgba(255, 255, 255, 0)" },
-    ],
     config: { duration: 1000 },
   });
 
-  const helpMenu = (
-    <Menu>
-      <Menu.ItemGroup title="Info">
-        <Menu.Item key="about">
-          <a
-            href="https://github.com/accordproject/template-playground/blob/main/README.md"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <QuestionOutlined /> About
-          </a>
-        </Menu.Item>
-        <Menu.Item key="community">
-          <a
-            href="https://discord.com/invite/Zm99SKhhtA"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <UserOutlined /> Community
-          </a>
-        </Menu.Item>
-        <Menu.Item key="issues">
-          <a
-            href="https://github.com/accordproject/template-playground/issues"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InfoOutlined /> Issues
-          </a>
-        </Menu.Item>
-      </Menu.ItemGroup>
-      <Menu.ItemGroup title="Documentation">
-        <Menu.Item key="documentation">
-          <a
-            href="https://github.com/accordproject/template-engine/blob/main/README.md"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BookOutlined /> Documentation
-          </a>
-        </Menu.Item>
-      </Menu.ItemGroup>
-    </Menu>
-  );
+  const menuItems = [
+    {
+      key: "about",
+      label: (
+        <a
+          href="https://github.com/accordproject/template-playground/blob/main/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <QuestionOutlined /> About
+        </a>
+      ),
+    },
+    {
+      key: "community",
+      label: (
+        <a
+          href="https://discord.com/invite/Zm99SKhhtA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <UserOutlined /> Community
+        </a>
+      ),
+    },
+    {
+      key: "issues",
+      label: (
+        <a
+          href="https://github.com/accordproject/template-playground/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InfoOutlined /> Issues
+        </a>
+      ),
+    },
+    {
+      key: "documentation",
+      label: (
+        <a
+          href="https://github.com/accordproject/template-engine/blob/main/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BookOutlined /> Documentation
+        </a>
+      ),
+    },
+  ];
 
   const menuItemStyle = (key: string, isLast: boolean) => ({
     display: "flex",
@@ -115,7 +130,7 @@ function Navbar({ scrollToFooter }: { scrollToFooter: any }) {
           style={{ display: "flex", alignItems: "center" }}
         >
           <Image
-            src={screens.lg ? "/logo.png" : "/accord_logo.png"}
+            src={screens.md ? "/logo.png" : "/accord_logo.png"}
             alt="Template Playground"
             preview={false}
             style={{
@@ -148,7 +163,7 @@ function Navbar({ scrollToFooter }: { scrollToFooter: any }) {
             onMouseEnter={() => setHovered("help")}
             onMouseLeave={() => setHovered(null)}
           >
-            <Dropdown overlay={helpMenu} trigger={["click"]}>
+            <Dropdown menu={{ items: menuItems }} trigger={["click"]}>
               <Button
                 style={{
                   background: "transparent",
