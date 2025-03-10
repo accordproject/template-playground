ca3d0b588f1714232bfca28e92b72aa321037dc9
Sahil Khan <sahilkhan123098p@gmail.com>
2025-03-10 16:34:35 +0530

    comment out tailwind style link
    Signed-off-by: Sahil Khan <sahilkhan123098p@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import "./styles/tailwind.css"
+// import "./styles/tailwind.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
