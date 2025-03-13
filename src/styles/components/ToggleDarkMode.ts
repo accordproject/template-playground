170fdd2daf0c7a166465b3c95e99f309b96fd8cc
Dharma Teja <dteja2468@gmail.com>
2025-03-13 16:14:00 +0530

    fix(button): fix dark mode toggle click - I201
    Signed-off-by: Dharma Teja <dteja2468@gmail.com>

--- src/styles/components/ToggleDarkMode.ts
+++ src/styles/components/ToggleDarkMode.ts
@@ -4,12 +4,14 @@ export const ToggleDarkModeContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 1rem; /* Using relative units for better responsiveness */
+  padding: 1rem; 
   
   /* Prevent clicks on the outer container */
   pointer-events: none;
 
   .dark-mode-toggle {
     pointer-events: auto; /* Enable clicks only inside the actual toggle */
+    outline: 0.125rem solid white;
+    border-radius: 1.25rem;
   }
 `;
