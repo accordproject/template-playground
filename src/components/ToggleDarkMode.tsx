6bbe01469016981c40bb01b33f4dc8b700061312
Dharma Teja <dteja2468@gmail.com>
2025-03-14 03:02:25 +0530

    fix(button): fix dark mode toggle click - I201
    Signed-off-by: Dharma Teja <dteja2468@gmail.com>

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -24,7 +24,7 @@ const ToggleDarkMode: React.FC = () => {
         className="dark-mode-toggle"
         onChange={handleChange}
         checked={isDarkMode}
-        size={60}
+        size={50}
       />
     </ToggleDarkModeContainer>
   );
