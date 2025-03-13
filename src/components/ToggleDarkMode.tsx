ec9c35d3fc49b30346ff4bd6738c926e818e393d
Dharma Teja <dteja2468@gmail.com>
2025-03-14 04:29:16 +0530

    fix(button): fix dark mode toggle click - I201
    Signed-off-by: Dharma Teja <dteja2468@gmail.com>

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -1,30 +1,20 @@
-import React, { useEffect, useState } from "react";
 import { ToggleDarkModeContainer } from "../styles/components/ToggleDarkMode";
 import DarkModeToggle from "react-dark-mode-toggle";
 import useAppStore from "../store/store";
 
 const ToggleDarkMode: React.FC = () => {
   const { backgroundColor, toggleDarkMode } = useAppStore();
-  const [isDarkMode, setIsDarkMode] = useState(backgroundColor === "#121212");
-
-  useEffect(() => {
-    setIsDarkMode(backgroundColor === "#121212");
-  }, [backgroundColor]);
-
-  const handleChange = () => {
-    toggleDarkMode();
-    setIsDarkMode(isChecked);
-  };
+  const isDarkMode = backgroundColor === "#121212";
 
   return (
     <ToggleDarkModeContainer>
-      <DarkModeToggle
-        className="dark-mode-toggle"
-        onChange={handleChange}
-        checked={isDarkMode}
-        size={50}
-      />
-    </ToggleDarkModeContainer>
+    <DarkModeToggle
+      className="dark-mode-toggle"
+      onChange={toggleDarkMode}
+      checked={isDarkMode}
+      size={50}
+    />
+  </ToggleDarkModeContainer>
   );
 };
 
