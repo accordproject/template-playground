a44e558aba94c30b77dc0445e8b151a350c9a5e5
surya4419 <suryaa4419@gmail.com>
2025-03-16 11:02:20 +0530

    fix(ui): add proper margin to error notification for consistent spacing
    Signed-off-by: surya4419 <suryaa4419@gmail.com>

--- src/utils/helpers/Errors.tsx
+++ src/utils/helpers/Errors.tsx
@@ -4,7 +4,7 @@ import { Alert, Space } from "antd";
 function Errors() {
   const error = useAppStore((state) => state.error);
   return error ? (
-    <Space direction="vertical" style={{ width: "100%" }}>
+    <Space direction="vertical" style={{ width: "100%", margin: "24px 0 0 24px" }}>
       <Alert message={error} type="error" />
     </Space>
   ) : (
