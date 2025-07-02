0874c5921d87cc9bf21f59416e09baae6743445a
Muskan B <muskanbararia@gmail.com>
2025-07-02 19:29:52 +0530

    Added Fixes for shadows and BGColors (#382)
    * fix:shadow fixes
    
    Signed-off-by: unknown <muskanbararia@gmail.com>
    
    * fix:shadow fixes
    
    Signed-off-by: unknown <muskanbararia@gmail.com>
    
    ---------
    
    Signed-off-by: unknown <muskanbararia@gmail.com>

--- src/pages/LearnNow.tsx
+++ src/pages/LearnNow.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
-import { LearnNowContainer, SidebarContainer, ContentContainer } from "../styles/pages/LearnNow";
+import { LearnNowContainer, ContentContainer } from "../styles/pages/LearnNow";
 import { steps } from "../constants/learningSteps/steps";
 
 const LearnNow: React.FC = () => {
   return (
     <LearnNowContainer>
-      <SidebarContainer>
         <Sidebar steps={steps} />
-      </SidebarContainer>
       <ContentContainer>
         <Outlet />
       </ContentContainer>
