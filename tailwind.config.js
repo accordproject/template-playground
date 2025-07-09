2f33c25897f5fd3ef416bf6a9e79d5f44f990bbc
nitro56565 <104414141+nitro56565@users.noreply.github.com>
2025-07-09 13:17:55 +0530

    tailwind integration added with preflight as false
    Signed-off-by: nitro56565 <104414141+nitro56565@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
