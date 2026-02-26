c6341ccfbf01e6d65eccd86c6d86a7edc0a0cb66
Matt Roberts <7544022+mttrbrts@users.noreply.github.com>
2026-02-26 10:48:16 +0530

    DCO Remediation Commit for Matt Roberts <7544022+mttrbrts@users.noreply.github.com>
    I, Matt Roberts <7544022+mttrbrts@users.noreply.github.com>, hereby add my Signed-off-by to this commit: 78ef327871cc19f7c4cc9dd1f640636c98b72e82
    I, Matt Roberts <7544022+mttrbrts@users.noreply.github.com>, hereby add my Signed-off-by to this commit: 85090eab09f855d87800d2644669974e9e37ebe6
    
    Signed-off-by: Matt Roberts <7544022+mttrbrts@users.noreply.github.com>

--- check-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-try {
-    const data = fs.readFileSync('package-lock.json', 'utf8');
-    JSON.parse(data);
-    console.log("VALID JSON");
-} catch (e) {
-    console.log("INVALID JSON", e.message);
-}
